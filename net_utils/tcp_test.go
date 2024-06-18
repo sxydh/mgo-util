afce6cbc17061bf2ae329d2947d40994d0024d16
sxydh <sxydhgg@gmail.com>
2024-06-18 18:28:25 +0800

    [UPDATE]

---
+++ net_utils/tcp_test.go
@@ -0,0 +1,82 @@
+package net_utils
+
+import (
+	"log"
+	"net"
+	"testing"
+)
+
+func TestTcpServer_TcpServerOnRand(t *testing.T) {
+	type fields struct {
+		OnConn    func(conn *net.Conn)
+		OnMessage func(msg string)
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{
+			name: "Normal",
+			fields: fields{
+				OnConn: func(conn *net.Conn) {
+					log.Printf("Get connection: localAddr=%v, remoteAddr=%v", (*conn).LocalAddr(), (*conn).RemoteAddr())
+				},
+				OnMessage: func(msg string) {
+					log.Printf("Get message: msg=%v", msg)
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := &TcpServer{
+				OnConn:    tt.fields.OnConn,
+				OnMessage: tt.fields.OnMessage,
+			}
+			server.TcpServerOnRand()
+		})
+	}
+}
+
+func TestTcpServer_TcpServerOnPort(t *testing.T) {
+	type fields struct {
+		OnConn    func(conn *net.Conn)
+		OnMessage func(msg string)
+	}
+	type args struct {
+		port int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Normal",
+			fields: fields{
+				OnConn: func(conn *net.Conn) {
+					log.Printf("Get connection: localAddr=%v, remoteAddr=%v", (*conn).LocalAddr(), (*conn).RemoteAddr())
+				},
+				OnMessage: func(msg string) {
+					log.Printf("Get message: msg=%v", msg)
+				},
+			},
+			args: args{
+				port: 30010,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := &TcpServer{
+				OnConn:    tt.fields.OnConn,
+				OnMessage: tt.fields.OnMessage,
+			}
+			if err := server.TcpServerOnPort(tt.args.port); (err != nil) != tt.wantErr {
+				t.Errorf("TcpServerOnPort() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
