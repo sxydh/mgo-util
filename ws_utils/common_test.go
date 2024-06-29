f0a4a111f0bb0a9c5c87246632c17a5b257ffdfc
sxydh <sxydhgg@gmail.com>
2024-06-29 18:23:10 +0800

    [UPDATE]

--- ws_utils/common_test.go
+++ ws_utils/common_test.go
@@ -1,6 +1,7 @@
 package ws_utils
 
 import (
+	"github.com/gorilla/websocket"
 	"log"
 	"testing"
 )
@@ -15,7 +16,13 @@ func TestWsServer_RandPort(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			server := &WsServer{}
+			server := &WsServer{
+				OnConn: func(conn *websocket.Conn) {
+				},
+				OnMessage: func(msg string) {
+					log.Printf("OnMessage: msg=%v", msg)
+				},
+			}
 			got := server.RandPort()
 			log.Printf("RandPort: got=%v", got)
 			select {}
