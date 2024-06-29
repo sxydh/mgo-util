dea77311887a38167a330a32900eb141cc2a4682
sxydh <sxydhgg@gmail.com>
2024-06-29 17:17:26 +0800

    [UPDATE]

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sxydh/mgo-util
 go 1.22.1
 
 require (
+	github.com/gorilla/websocket v1.5.3
 	golang.org/x/crypto v0.24.0
 )
 
---
+++ ws_utils/common.go
@@ -0,0 +1,63 @@
+package ws_utils
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type WsServer struct {
+	OnMessage func(msg string)
+}
+
+func (server *WsServer) RandPort() int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for {
+		port := 40000 + r.Intn(10000)
+		listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+		if err != nil {
+			continue
+		}
+		_ = listener.Close()
+		go func() {
+			server.Port(port)
+		}()
+		return port
+	}
+}
+
+func (server *WsServer) Port(port int) {
+	http.HandleFunc("/", server.wsHandler)
+	log.Printf("ListenAndServe going: port=%v", port)
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	if err != nil {
+		log.Printf("ListenAndServe error: err=%v", err)
+	}
+}
+
+//goland:noinspection GoUnhandledErrorResult
+func (server *WsServer) wsHandler(w http.ResponseWriter, r *http.Request) {
+	var upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	wsConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Upgrade error: err=%v", err)
+		return
+	}
+	defer wsConn.Close()
+	for {
+		_, p, err := wsConn.ReadMessage()
+		if err != nil {
+			log.Printf("ReadMessage error: err=%v", err)
+			return
+		}
+		server.OnMessage(string(p))
+	}
+}
