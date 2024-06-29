c3b41e30059fa9c64b660a9dfe92048ac3f0d674
sxydh <sxydhgg@gmail.com>
2024-06-29 17:03:35 +0800

    [UPDATE]

---
+++ ws_utils/common_test.go
@@ -0,0 +1,23 @@
+package ws_utils
+
+import (
+	"log"
+	"testing"
+)
+
+func TestWsServer_RandPort(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "Normal",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := &WsServer{}
+			got := server.RandPort()
+			log.Printf("RandPort: got=%v", got)
+		})
+	}
+}
